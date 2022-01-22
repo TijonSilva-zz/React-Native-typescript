697e35ad043fed4d66e607bda69d1f430308df34
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2022-01-22 20:22:07 -0300

    dependencias types ok

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import SignIn from './pages/Signin';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <SignIn />
+    </SafeAreaView>
+  );
+};
+
+export default App;
